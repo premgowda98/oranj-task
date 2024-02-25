46ec4b42c9d14b145c3745afc5977e2a2f5c5ca5
Prem Kumar G <premkumarmvks@gmail.com>
2024-02-25 16:30:26 +0530

    bug: clearError fixed

--- error.vue
+++ error.vue
@@ -2,7 +2,7 @@
     <NuxtLayout >
         <div class="mt-7 max-w-lg mx-auto text-center ">
             <p class="mt-7 text-5xl font-bold">{{error.statusCode}}</p>
-            <p class="mt-7 text-3xl">{{error.message}}</p>
+            <p class="mt-7 text-3   xl">{{error.message}}</p>
             <button class="bg-blue-500 my-7 text-3xl text-white p-2 rounded-lg hover:bg-white hover:text-blue-500 border-2" @click="handleError">Go Home...</button>
         </div>
 
@@ -14,5 +14,8 @@
       error: Object
     })
   
-    const handleError = () => clearError({ redirect: '/' })
+    const handleError = () => {
+        clearError() // clearError({redirect:'/'}) will work outside of NuxtLayout
+        navigateTo('/')
+    }
   </script>
